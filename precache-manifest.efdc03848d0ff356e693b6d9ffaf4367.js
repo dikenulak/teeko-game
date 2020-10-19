be3933174c6ab80450b4bf83b08988a3f54c3161
Diken Ulak <diken@evolveasia.co>
2020-10-19 18:55:21 +0545

    Updates

---
+++ precache-manifest.efdc03848d0ff356e693b6d9ffaf4367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6492fd29168331097598fa4f2d6418c",
+    "url": "/teeko-game/index.html"
+  },
+  {
+    "revision": "7a02e63074bc173e7ef6",
+    "url": "/teeko-game/static/css/main.86b1b8a1.chunk.css"
+  },
+  {
+    "revision": "e1af777db024617bacea",
+    "url": "/teeko-game/static/js/2.e2c8829a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teeko-game/static/js/2.e2c8829a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a02e63074bc173e7ef6",
+    "url": "/teeko-game/static/js/main.d5982966.chunk.js"
+  },
+  {
+    "revision": "45f0a6c5f46937fcc7e5",
+    "url": "/teeko-game/static/js/runtime-main.fb2ca7ea.js"
+  }
+]);
